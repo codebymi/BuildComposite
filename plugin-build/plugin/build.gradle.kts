0765a52f46e8f321e4752eef266e3cb0e78f98ea
codebymi <103326133+codebymi@users.noreply.github.com>
2022-07-03 19:45:47 +0700

    Initial commit

---
+++ plugin-build/plugin/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    kotlin("jvm")
+    id("java-gradle-plugin")
+    id("com.gradle.plugin-publish")
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk7"))
+    implementation(gradleApi())
+
+    testImplementation(libs.junit)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+gradlePlugin {
+    plugins {
+        create(PluginCoordinates.ID) {
+            id = PluginCoordinates.ID
+            implementationClass = PluginCoordinates.IMPLEMENTATION_CLASS
+            version = PluginCoordinates.VERSION
+            displayName = PluginBundle.DISPLAY_NAME
+        }
+    }
+}
+
+// Configuration Block for the Plugin Marker artifact on Plugin Central
+pluginBundle {
+    website = PluginBundle.WEBSITE
+    vcsUrl = PluginBundle.VCS
+    description = PluginBundle.DESCRIPTION
+    tags = PluginBundle.TAGS
+}
+
+tasks.create("setupPluginUploadFromEnvironment") {
+    doLast {
+        val key = System.getenv("GRADLE_PUBLISH_KEY")
+        val secret = System.getenv("GRADLE_PUBLISH_SECRET")
+
+        if (key == null || secret == null) {
+            throw GradleException("gradlePublishKey and/or gradlePublishSecret are not defined environment variables")
+        }
+
+        System.setProperty("gradle.publish.key", key)
+        System.setProperty("gradle.publish.secret", secret)
+    }
+}
